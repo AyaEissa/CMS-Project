4f27e49879e51cfe0756cebcf65b2706d614ded7
AyaEissa <Ayaahmedeissa@gmail.com>
2019-03-31 17:17:03 +0200

    add volunteer table

--- CMS-Script.sql
+++ CMS-Script.sql
@@ -3,3 +3,13 @@ DROP TABLE Branch cascade constraints;
 CREATE TABLE Branch
 (BID number(3) primary key,
 Address varchar2(50) not null);
+
+CREATE TABLE Volunteer 
+   (	"SSN" NUMBER(10,0) NOT NULL primary key, 
+	"FNAME" VARCHAR2(10 BYTE) NOT NULL, 
+	"LNAME" VARCHAR2(10 BYTE)NOT NULL, 
+	"SEX" CHAR(1 BYTE), 
+	"EMAIL" VARCHAR2(20 BYTE), 
+	"MOBILE_NUMBER" NUMBER(12,0) NOT NULL, 
+	"ADDRESS" VARCHAR2(50 BYTE)
+   )
