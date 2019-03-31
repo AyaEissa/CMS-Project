5f7cac3ea63646ad54bcfe501487442a8d549c71
polamokh <polamokh10@hotmail.com>
2019-03-31 20:13:17 +0200

    Add Campaign table and fix tables columns names.

--- CMS-Script.sql
+++ CMS-Script.sql
@@ -3,37 +3,45 @@ DROP TABLE Volunteer cascade constraints;
 DROP TABLE Donor cascade constraints;
 DROP TABLE Activity cascade constraints;
 DROP TABLE Donates cascade constraints;
+DROP TABLE Campaign cascade constraints;
 
 CREATE TABLE Branch
 (BID number(3) primary key,
 Address varchar2(50) not null);
 
 CREATE TABLE Volunteer 
-("SSN" NUMBER(10,0) primary key, 
-"FNAME" VARCHAR2(10) NOT NULL, 
-"LNAME" VARCHAR2(10) NOT NULL, 
-"SEX" CHAR(1), 
-"EMAIL" VARCHAR2(20), 
-"MOBILE_NUMBER" NUMBER(12,0) NOT NULL, 
-"ADDRESS" VARCHAR2(50));
+(SSN NUMBER(10,0) primary key, 
+FNAME VARCHAR2(10) NOT NULL, 
+LNAME VARCHAR2(10) NOT NULL, 
+SEX CHAR(1), 
+EMAIL VARCHAR2(20), 
+MOBILENUMBER NUMBER(12,0) NOT NULL, 
+ADDRESS VARCHAR2(50));
 
 CREATE TABLE Donor 
-("SSN" NUMBER(10,0) primary key, 
-"FNAME" VARCHAR2(10) NOT NULL, 
-"LNAME" VARCHAR2(10) NOT NULL, 
-"SEX" CHAR(1), 
-"EMAIL" VARCHAR2(20), 
-"MOBILE_NUMBER" NUMBER(12,0) NOT NULL, 
-"ADDRESS" VARCHAR2(50));
+(SSN NUMBER(10,0) primary key, 
+FNAME VARCHAR2(10) NOT NULL, 
+LNAME VARCHAR2(10) NOT NULL, 
+SEX CHAR(1), 
+EMAIL VARCHAR2(20), 
+MOBILENUMBER NUMBER(12,0) NOT NULL, 
+ADDRESS VARCHAR2(50));
 
 CREATE TABLE Activity
-("ANAME" VARCHAR(50) PRIMARY KEY,
-"BID" NUMBER(3) references Branch(BID));
+(ActName VARCHAR(50) PRIMARY KEY,
+BID NUMBER(3) references Branch(BID));
 
 CREATE TABLE Donates
-("DON_SSN" NUMBER(10,0) references Donor(SSN),
-"Activity_Name" VARCHAR(50) references Activity(ANAME),
-"Code" VARCHAR(25),
-"Date" DATE NOT NULL,
-"Time" DATE NOT NULL,
-"Amount" Number(10) NOT NULL);
+(DONSSN NUMBER(10,0) references Donor(SSN),
+ActivityName VARCHAR(50) references Activity(ActName),
+Code VARCHAR(25),
+DonDate DATE NOT NULL,
+DonTime DATE NOT NULL,
+Amount Number(10) NOT NULL);
+
+CREATE TABLE Campaign
+(CampName varchar2(30),
+CampDate date,
+BID number(3) references Branch(BID),
+ActivityName varchar2(50) references Activity(ActName),
+constraint pkCamp primary key (CampDate, BID, ActivityName));
