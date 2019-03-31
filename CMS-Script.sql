655768927fdd503e195428d477b8075d68650949
polamokh <polamokh10@hotmail.com>
2019-03-31 20:06:04 +0200

    Revert "Add Campaign table and fix some tables columns names."
    This reverts commit 8dcd9c2b860308feb58dcea9b8ac0a86bd4d0224.

--- CMS-Script.sql
+++ CMS-Script.sql
@@ -2,7 +2,6 @@ DROP TABLE Branch cascade constraints;
 DROP TABLE Volunteer cascade constraints;
 DROP TABLE Donor cascade constraints;
 DROP TABLE Activity cascade constraints;
-DROP TABLE Campaign cascade constraints;
 
 CREATE TABLE Branch
 (BID number(3) primary key,
@@ -14,7 +13,7 @@ CREATE TABLE Volunteer
 "LNAME" VARCHAR2(10) NOT NULL, 
 "SEX" CHAR(1), 
 "EMAIL" VARCHAR2(20), 
-"MOBILENUMBER" NUMBER(12,0) NOT NULL, 
+"MOBILE_NUMBER" NUMBER(12,0) NOT NULL, 
 "ADDRESS" VARCHAR2(50));
 
 CREATE TABLE Donor 
@@ -23,16 +22,9 @@ CREATE TABLE Donor
 "LNAME" VARCHAR2(10) NOT NULL, 
 "SEX" CHAR(1), 
 "EMAIL" VARCHAR2(20), 
-"MOBILENUMBER" NUMBER(12,0) NOT NULL, 
+"MOBILE_NUMBER" NUMBER(12,0) NOT NULL, 
 "ADDRESS" VARCHAR2(50));
 
 CREATE TABLE Activity
-(ActName VARCHAR2(50) PRIMARY KEY,
-BID NUMBER(3) references Branch(BID));
-
-CREATE TABLE Campaign
-(CampName varchar2(30),
-CampDate date,
-BID number(3) references Branch(BID),
-ActivityName varchar2(50) references Activity(ActName),
-constraint pkCamp primary key (CampDate, BID, ActivityName));
+("Name" VARCHAR(50) PRIMARY KEY,
+"BID" NUMBER(3) references Branch(BID));
