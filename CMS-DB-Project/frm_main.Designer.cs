3ed12b32186c3ddf5063823c57332c150a97d1ad
polamokh <polamokh10@hotmail.com>
2019-04-09 14:28:44 +0200

    Add new form for Volunteer table and insert new line "COMMIT" in SQL code.

---
+++ CMS-DB-Project/frm_main.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CMS_DB_Project
+{
+    partial class frm_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.volunteerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageVolunteerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.volunteerToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(605, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "Menu Strip";
+            // 
+            // volunteerToolStripMenuItem
+            // 
+            this.volunteerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageVolunteerToolStripMenuItem});
+            this.volunteerToolStripMenuItem.Name = "volunteerToolStripMenuItem";
+            this.volunteerToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.volunteerToolStripMenuItem.Text = "Volunteer";
+            // 
+            // manageVolunteerToolStripMenuItem
+            // 
+            this.manageVolunteerToolStripMenuItem.Name = "manageVolunteerToolStripMenuItem";
+            this.manageVolunteerToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.manageVolunteerToolStripMenuItem.Text = "Manage";
+            this.manageVolunteerToolStripMenuItem.Click += new System.EventHandler(this.manageVolunteerToolStripMenuItem_Click);
+            // 
+            // frm_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 385);
+            this.Controls.Add(this.menuStrip);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "frm_main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Menu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem volunteerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageVolunteerToolStripMenuItem;
+    }
+}
---
+++ CMS-DB-Project/frm_main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMS_DB_Project
+{
+    public partial class frm_main : Form
+    {
+        public frm_main()
+        {
+            InitializeComponent();
+        }
+
+        private void manageVolunteerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_manage_volunteer volunteerForm = new frm_manage_volunteer();
+            volunteerForm.MdiParent = this;
+            volunteerForm.Show();
+        }
+    }
+}
